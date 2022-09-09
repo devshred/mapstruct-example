6e290e917d24b10070946c2f5a322f17ddeebab7
Johannes Schmidt <devshred@tigerflanke.de>
2022-09-09 09:57:43 +0200

    use InheritInverseConfiguration

--- src/main/java/org/devshred/mapstruct/customer/CustomerMapper.java
+++ src/main/java/org/devshred/mapstruct/customer/CustomerMapper.java
@@ -8,11 +8,12 @@ import org.mapstruct.Mapping;
 @Mapper(imports = UUID.class)
 public interface CustomerMapper {
     @Mapping(source = "customerId", target = "id")
-//    @Mapping(target = "validFrom", dateFormat = "dd.MM.yyyy")
     @Mapping(source = "address.city", target = "city")
+    // @Mapping(target = "startDate", dateFormat = "dd.MM.yyyy")
+    // @InheritInverseConfiguration
     CustomerDto entityToDto(CustomerEntity entity);
 
-    @Mapping(source = "id", target = "customerId", defaultExpression = "java(UUID.randomUUID())")
+    @Mapping(target = "customerId", source = "id", defaultExpression = "java(UUID.randomUUID())")
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "address.city", source = "city")
     CustomerEntity dtoToEntity(CustomerDto dto);
